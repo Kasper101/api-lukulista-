0de7bb0fc10688a9d98dbef0e8393097e53aed34
Kasper101 <75790140+Kasper101@users.noreply.github.com>
2021-10-08 14:18:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
+function arvoNumero(alaraja, ylaraja) {
+  return Math.floor(ylaraja * math.random())
+
+  
 app.get('/', function (req, res)  {
-  res.send('1 5 8 2 5')
+  let NUMEROLISTA = [
+  arvoNumero(0,5),
+  arvoNumero(10,15),
+  arvoNumero(100,500),
+  arvoNumero(10,20),
+  arvoNumero(0.5)
+  res.send(numerolista)]
 })
 
 app.listen(PORT)
